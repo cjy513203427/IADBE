c1dd98318ecb4607f1ecb058c85d905065fb9086
jinyao <634208959@qq.com>
2024-05-03 12:25:17 +0200

    Train and test with fastflow

--- train_test_with_padim.py
+++ train_test_with_padim.py
@@ -3,23 +3,14 @@ from anomalib.data import MVTec
 from anomalib.engine import Engine
 from anomalib.models import Padim
 
-datamodule = MVTec(num_workers=0)
-datamodule.prepare_data()  # Downloads the datasets if it's not in the specified `root` directory
-datamodule.setup()  # Create train/val/test/prediction sets.
+
 datasets=['screw', 'pill', 'capsule', 'carpet', 'grid', 'tile', 'wood', 'zipper', 'cable', 'toothbrush', 'transistor', 'metal_nut', 'bottle', 'hazelnut', 'leather']
 
 
 model = Padim()
 
 for dataset in datasets:
-    datamodule = MVTec(
-        category=dataset,
-        image_size=256,
-        train_batch_size=32,
-        eval_batch_size=32,
-        num_workers=8,
-        task=TaskType.SEGMENTATION,
-    )
+    datamodule = MVTec(category=dataset, num_workers=0)
 
     engine = Engine(task=TaskType.SEGMENTATION)
     engine.fit(model=model, datamodule=datamodule)
