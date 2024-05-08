8b41148bf406ea3d7b137ecc7b0cbf7a279f018d
jinyao <634208959@qq.com>
2024-05-08 11:45:08 +0200

    1. fine tuning train code 2. add logger

--- train_test_with_padim.py
+++ train_test_with_padim.py
@@ -1,18 +1,29 @@
+import logging
 from anomalib import TaskType
 from anomalib.data import MVTec
 from anomalib.engine import Engine
 from anomalib.models import Padim
 
+# configure logger
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
 datasets = ['screw', 'pill', 'capsule', 'carpet', 'grid', 'tile', 'wood', 'zipper', 'cable', 'toothbrush', 'transistor',
             'metal_nut', 'bottle', 'hazelnut', 'leather']
 
 for dataset in datasets:
+    logger.info(f"================== Processing dataset: {dataset} ==================")
     model = Padim()
-    datamodule = MVTec(category=dataset, num_workers=0)
+    datamodule = MVTec(category=dataset, num_workers=0, train_batch_size=256,
+                       eval_batch_size=256)
     # metrics is under "anomalib/metrics/"
-    engine = Engine(pixel_metrics=["AUROC", "PRO"], task=TaskType.SEGMENTATION)
+    # engine = Engine(pixel_metrics=["AUROC", "AUPR", "PRO"], image_metrics=["AUROC", "AUPR", "PRO"], task=TaskType.SEGMENTATION)
+    engine = Engine(pixel_metrics=["AUROC", "PRO"], image_metrics=["AUROC", "PRO"], task=TaskType.SEGMENTATION)
+
+    logger.info(f"================== Start training for dataset: {dataset} ==================")
     engine.fit(model=model, datamodule=datamodule)
 
+    logger.info(f"================== Start testing for dataset: {dataset} ==================")
     test_results = engine.test(
         model=model,
         datamodule=datamodule,
