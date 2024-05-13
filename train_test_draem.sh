93371127ea3f5635bf0d346a3f13d309711a9b25
jinyao <634208959@qq.com>
2024-05-13 10:09:37 +0200

    1. add metrics for .yaml. 2. test training codes. 3. delete not executable api code.

---
+++ train_test_draem.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+datasets=('screw' 'pill' 'capsule' 'carpet' 'grid' 'tile' 'wood' 'zipper' 'cable' 'toothbrush' 'transistor' 'metal_nut' 'bottle' 'hazelnut' 'leather')
+config_file="./configs/draem.yaml"
+
+for dataset in "${datasets[@]}"
+do
+    command="anomalib train --data anomalib.data.MVTec --data.category $dataset --config $config_file"
+    echo "Running command: $command"
+    # Excute command
+    $command
+done
