536da10d1661911be95100d8b38c2a38592abb75
jinyao <634208959@qq.com>
2024-05-31 17:16:23 +0200

    Add logprocessing for dealing with logs of training with bash

---
+++ logs/logprocessing_py_latex_table.py
@@ -0,0 +1,65 @@
+import re
+
+# processing raw logs, extract metric values and concatenate string.
+with open('rawlogs/train_test_mvtec_uflow.log', 'r') as file:
+    log_data = file.read()
+
+
+pattern = re.compile(
+    r"Start testing for dataset: (.*?)\n"  # 匹配数据集名称
+    r"[\s\S]*?"  # 匹配任何字符（包括换行符）零次或多次
+    r"┏━━━━━━━━━━━━━━━━━━━━━━━━━━━┳━━━━━━━━━━━━━━━━━━━━━━━━━━━┓\n"
+    r"┃        Test metric        ┃       DataLoader 0        ┃\n"
+    r"┡━━━━━━━━━━━━━━━━━━━━━━━━━━━╇━━━━━━━━━━━━━━━━━━━━━━━━━━━┩\n"
+    r"│        image_AUROC        │\s+([0-9.]+)\s+│\n"  # 匹配 image_AUROC 值
+    r"│         image_PRO         │\s+([0-9.]+)\s+│\n"  # 匹配 image_PRO 值
+    r"│        pixel_AUROC        │\s+([0-9.]+)\s+│\n"  # 匹配 pixel_AUROC 值
+    r"│         pixel_PRO         │\s+([0-9.]+)\s+│\n"  # 匹配 pixel_PRO 值
+    r"└───────────────────────────┴───────────────────────────┘"
+)
+
+matches = pattern.findall(log_data)
+
+dataset_names = []
+image_AUROC_values = []
+image_PRO_values = []
+pixel_AUROC_values = []
+pixel_PRO_values = []
+
+for match in matches:
+    dataset_name = match[0]
+    image_AUROC = match[1]
+    image_PRO = match[2]
+    pixel_AUROC = match[3]
+    pixel_PRO = match[4]
+
+    dataset_names.append(dataset_name)
+    image_AUROC_values.append(round(float(image_AUROC) * 100, 2))
+    image_PRO_values.append(round(float(image_PRO) * 100, 2))
+    pixel_AUROC_values.append(round(float(pixel_AUROC) * 100, 2))
+    pixel_PRO_values.append(round(float(pixel_PRO) * 100, 2))
+
+
+# Print the extracted values
+for i in range(len(matches)):
+    print()
+    print(f"Dataset: {dataset_names[i]}")
+    print(f"Test metric          Result")
+    print(f"----------------------------------")
+    print(f"image_AUROC          {image_AUROC_values[i]:.2f}")
+    print(f"image_PRO            {image_PRO_values[i]:.2f}")
+    print(f"pixel_AUROC          {pixel_AUROC_values[i]:.2f}")
+    print(f"pixel_PRO            {pixel_PRO_values[i]:.2f}")
+
+# Prepare LaTeX string
+latex_values = " & ".join([f"{value:.2f}" for value in image_AUROC_values])
+print(f"LaTeX string: {latex_values}")
+
+
+# Print the counts
+print(f"Number of matches: {len(matches)}")
+print(f"Number of dataset names: {len(dataset_names)}")
+print(f"Number of image_AUROC values: {len(image_AUROC_values)}")
+print(f"Number of image_PRO values: {len(image_PRO_values)}")
+print(f"Number of pixel_AUROC values: {len(pixel_AUROC_values)}")
+print(f"Number of pixel_PRO values: {len(pixel_PRO_values)}")
---
+++ train_test_visa_dsr.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# shellcheck disable=SC2054
+datasets=("candle"
+            "capsules"
+            "cashew"
+            "chewinggum"
+            "fryum"
+            "macaroni1"
+            "macaroni2"
+            "pcb1"
+            "pcb2"
+            "pcb3"
+            "pcb4"
+            "pipe_fryum")
+config_file="./configs/models/dsr.yaml"
+
+for dataset in "${datasets[@]}"
+do
+    command="anomalib train --data anomalib.data.Visa --data.category $dataset --config $config_file"
+    echo "Running command: $command"
+    # Excute command
+    $command
+done
