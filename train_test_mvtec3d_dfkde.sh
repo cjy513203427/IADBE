42cb718967b7a49380c21f614d8e309406b5422b
jinyao <634208959@qq.com>
2024-06-05 17:57:14 +0200

    bash for training with mvtec3d dataset

---
+++ train_test_mvtec3d_dfkde.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+datasets=("bagel" "cable_gland" "carrot" "cookie" "dowel" "foam" "peach" "potato" "rope" "tire")
+data_config_file="./configs/data/mvtec_3d.yaml"
+config_file="./configs/models/dfkde.yaml"
+
+for dataset in "${datasets[@]}"
+do
+    command="anomalib train --data $data_config_file --data.category $dataset --config $config_file"
+    echo "Running command: $command"
+    # Excute command
+    $command
+done
+
+
+
