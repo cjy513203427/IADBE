77dd2cc2b122a913fd2bb5bbc0f4b7a6aeabd3b0
jinyao <634208959@qq.com>
2024-07-16 12:00:49 +0200

    1. add sh and logs for fre model 2. update log and sh for kolektor dataset

---
+++ train_test_btech_fre.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+datasets=("01" "02" "03")
+data_config_file="./configs/data/btech.yaml"
+config_file="./configs/models/fre.yaml"
+
+for dataset in "${datasets[@]}"
+do
+    command="anomalib train --data $data_config_file --data.category $dataset --config $config_file"
+    echo "Running command: $command"
+    # Excute command
+    $command
+done
--- train_test_kolektor_fre.sh
+++ train_test_kolektor_fre.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 data_config_file="./configs/data/kolektor.yaml"
-config_file="./configs/models/draem.yaml"
+config_file="./configs/models/fre.yaml"
 
 command="anomalib train --data $data_config_file --config $config_file"
 echo "Running command: $command"
